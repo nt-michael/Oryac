43897453f592fb50e94b147767fb065834b71d77
Nde Mikel <ndemikel@gmail.com>
2016-06-04 22:16:16 +0100

    updates

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ORYAC | About Us...</title>
+	<title>ORYAC | Welcome Home...</title>
 	<link href="style.css" rel="stylesheet">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
   	<link rel="stylesheet" href="css/bootstrap.css">
@@ -24,16 +24,30 @@
 
 	<div class="container">	
 	<?php include('navbar2.php'); ?>
+		<div class="row" style="padding-top: 7%">
+				<div class="col-md-3" style="text-align: left;padding-left: 6%;">
+					<img src="" title="home ORYAC" alt="personality.jpg" style="width:100%;height: 200px">
+				</div>
+				<div class="col-md-9" style="text-align:justify;padding-left:10%">
+					<h2>Personality Of User</h2>
+					<p><ul>
+						<li>Learn more about your Academic Personality</li>
+						<li>What is trending about this personality</li>
+						<li>What next?...</li>
+						</ul>
+					</p>
+				</div>
+			</div>
 		<div class="row" style="padding-left: 5%">
-			<h2><strong>Team</strong></h2>
+			<h2 style="text-align: right; padding-right: 20%"><strong>Description</strong></h2>
 			<div style="padding-right: 20%">
 			<?php include('line.php'); ?>
 			</div>
 		</div>
-		<div class="row" style="">
+		<div class="row" style="padding-bottom: 5%">
 			<div class="row">
-				<div class="col-md-3" style="text-align: right;">
-					<img src="" title="Founder ORYAC" alt="founder.jpg" style="width:80%;height: 200px">
+				<div class="col-md-3" style="text-align: left;padding-left: 7%">
+					<img src="" title="home ORYAC" alt="founder.jpg" style="width:70%;height: 130px">
 				</div>
 				<div class="col-md-9" style="text-align:justify;padding-right: 20%">
 					<p>
@@ -57,25 +71,25 @@
 					</p>
 				</div>
 				<div class="col-md-5" style="text-align: left;padding-right: 15%">
-					<img src="" title="Co-founder ORYAC" alt="cofounder.jpg" style="width:80%;height: 200px">
-				</div>
-			</div>
-			<div class="row"><br>
-				<div class="col-md-3" style="text-align: right;">
-					<img src="" title="Founder ORYAC" alt="Collaborator.jpg" style="width:80%;height: 200px">
-				</div>
-				<div class="col-md-9" style="text-align:justify;padding-right: 20%">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-						incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-						nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-						aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-						deserunt mollit anim id est laborum.
-					</p>
+					<img src="" title="Co-founder ORYAC" alt="cofounder.jpg" style="width:60%;height: 130px">
 				</div>
 			</div>
 		</div>
+		<div class="row" style="padding-left: 5%">
+			<h2 style="text-align:;"><strong>What Are Majors</strong></h2>
+			<div style="padding-right: 20%">
+			<?php include('line.php'); ?>
+			</div>
+		</div>
+	</div>
+	<div class="row" style="text-align:justify;padding-left:10%">
+					<h2>Personality Of User</h2>
+					<p><ul>
+						<li>Learn more about your Academic Personality</li>
+						<li>What is trending about this personality</li>
+						<li>What next?...</li>
+						</ul>
+					</p>
 	</div>
 
 	<?php include('footer.php'); ?>
