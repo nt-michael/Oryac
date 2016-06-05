df989f73ca48cc35bc53e2b8013a35717ef2d31f
Nde Mikel <ndemikel@gmail.com>
2016-06-05 01:19:25 +0100

    profile, home, and major

---
+++ major.php
@@ -0,0 +1,154 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ORYAC | About Us...</title>
+	<link href="style.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+  	<link rel="stylesheet" href="css/bootstrap.css">
+  	<link rel="stylesheet" type="text/css" href="css/image.css">
+ 	<script src="../../ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  	<script src="js/bootstrap.min.js"></script>
+<style type="text/css">
+	hr.style1{
+	border-top: 1px solid #8c8b8b;
+	width: 100%;
+	height: 50px;
+}
+</style>
+
+</head>
+
+<body style="">
+
+	<?php //include('navbar2.php'); ?>
+
+	<div class="container">	
+	<?php include('navbar2.php'); ?>
+		<div style="text-align: right;padding-right: 20%;">
+			<a href="profile.php">View profile&nbsp;<span style="font-size: 40px" class="glyphicon glyphicon-user"></span></a>
+		</div>
+		<div class="row" style="text-align:justify;padding-left:5%;padding-bottom: 5%">
+			<p>Hello User_name...</p>
+		</div>
+		<div class="row" style="padding-left: 5%">
+			<h2><strong>Different Majors Pertaining To Your Personality</strong></h2>
+			<div style="padding-right: 20%">
+			<?php include('line.php'); ?>
+			</div>
+		</div>
+		<div class="row">
+				<div class="col-md-12" style="text-align: justify;padding-left: 6%;padding-right: 20%">
+					<p><br>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+						dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+						deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+		<div class="row" style="padding-left: 5%">
+			
+			<div class="row" style="margin-top: 10%;">
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(https://www.stopthehacker.com/wp-content/uploads/2013/01/Fotolia_48220935_Subscription_Monthly_L.jpg);">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major A</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+				<!-- Second major -->
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(http://affairstoday.co.uk/wp-content/uploads/2015/10/elibrary.jpg); ">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major B</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+				<!-- Third major -->
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(http://pad3.whstatic.com/images/thumb/9/95/Make-a-Study-Timetable-Step-7.jpg/aid676229-728px-Make-a-Study-Timetable-Step-7.jpg);">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major C</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+				<!-- Fourth major -->
+			</div>
+			<!-- Next line -->
+			<div class="row" style="">
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(http://www.study-habits.com/wp-content/uploads/2012/04/prepare-exams.jpg);">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major D</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+				<!-- Second major -->
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(http://www.lawsonstate.edu/sites/www/Uploads/images/Programs_Of_Study/Academic_Programs/finalex.jpg); ">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major E</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+				<!-- Third major -->
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(http://www.careerealism.com/wp-content/uploads/2012/10/Good-Resume-Featured.png);">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major F</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+			</div>
+				<!-- Next -->
+			<div class="row" style="">
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(http://www.study-habits.com/wp-content/uploads/2012/04/prepare-exams.jpg);">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major G</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+				<!-- Second major -->
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(http://www.lawsonstate.edu/sites/www/Uploads/images/Programs_Of_Study/Academic_Programs/finalex.jpg); ">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major H</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+				<!-- Third major -->
+				<div class="col-md-3">
+					<div class="module" style="background-image: url(http://www.careerealism.com/wp-content/uploads/2012/10/Good-Resume-Featured.png);">
+						<header>
+							<h3>
+								<a style="color:white" href="#">Sample Major J</a>
+							</h3>
+						</header>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include('footer.php'); ?>
+</body>
+</html>
--- profile.php
+++ profile.php
@@ -1,23 +1,107 @@
-<?php
-session_start(); // start the session
-include("connection.php");
 
-$user_id = $_SESSION["userid"]; // store the user id into session
-
-$sql = "SELECT * FROM tbl_user WHERE user_id='$user_id'";
-$result = mysql_query($sql);
-
-if($row = mysql_fetch_array($result)) {
-    $username = $row["username"];
-    $name = $row["name"];
-    $email = $row["email"];
-
-    echo "
-    <table>
-        <tr><td>User Name</td><td> : </td><td>$username</td></tr>
-        <tr><td>Name</td><td> : </td><td>$name</td></tr>
-        <tr><td>Email</td><td> : </td><td>$email</td></tr>
-    </table>
-    ";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ORYAC | About Us...</title>
+    <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="../../ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+<style type="text/css">
+    hr.style1{
+    border-top: 1px solid #8c8b8b;
+    width: 100%;
+    height: 50px;
 }
-?>
+</style>
+
+</head>
+
+<body style="">
+
+    <?php //include('navbar2.php'); ?>
+
+    <div class="container"> 
+    <?php include('navbar2.php'); ?>
+    <div style="text-align: right;padding-right: 20%;">
+            <a href="profile.php">Home&nbsp;<span style="font-size: 40px" class="glyphicon glyphicon-home"></span></a>
+        </div>
+        <div class="row" style="padding-left: 5%">
+            <h2><strong>My Profile</strong></h2>
+            <div style="padding-right: 20%">
+            <?php include('line.php'); ?>
+            </div>
+        </div>
+        <div class="row" style="">
+            <div class="row">
+            <div style="padding-left: 6%">
+                <div class="col-md-5" style="text-align: left;border:1px solid black;height: auto;padding: 2%">
+                    <label>Update your settings...</label><br>
+                    <form class="form-group" action="">
+                        <label class="form-group">First name:</label>
+                        <input class="form-control" name="fname" placeholder="firstname" type="text">
+
+                        <label class="form-group">Last name:</label>
+                        <input class="form-control" name="lname" placeholder="last name" type="text">
+
+                        <label class="form-group">BirthDate:</label>
+                        <input class="form-control" name="birth" placeholder="birthdate" type="date">
+
+                        <label class="form-group">Current Level:</label>
+                        <input class="form-control" name="level" placeholder="form level" type="text">
+
+                        <label class="form-group">Phone number:</label>
+                        <input class="form-control" name="tel1" placeholder="telephone1" type="number">
+
+                        <label class="form-group">Other number:</label>
+                        <input class="form-control" name="tel2" placeholder="telephone2" type="number">
+
+                        <label class="form-group">Current password:</label>
+                        <input class="form-control" name="past-psw" placeholder="current password" type="password">
+
+                        <label class="form-group">New password:</label>
+                        <input class="form-control" name="new-psw" placeholder="new password" type="password">
+
+                        <div style="text-align: center"><br>
+                            <input class="form-group btn btn-warning" type="submit" value="Update Settings">
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+                <div class="col-md-6" style="text-align:justify;padding-right: 12%;padding-left: 2%">
+                    <form class="form-group" action="">
+                        <label class="form-group">Background Info:</label>
+                        <textarea class="form-control" name="bg-info" placeholder="Background Info" type="text" style="height: 200px;border:1px solid black; resize: none"></textarea>
+                        <br><input class="form-group btn btn-warning" type="submit" value="Save information">
+                    </form><br><br>
+
+                    <form style="text-align: right" class="form-group" action="#">
+                        <label class="">View Past Test:</label>
+
+                            <input class="form-group btn btn-warning" type="submit" value="Click here...">
+
+                    </form><br><br>
+
+                    <form class="form-group" action="" style="border:1px solid black;padding: 2%">
+                        <legend>Update Just Password...</legend>
+                        <label class="form-group">Current password:</label>
+                        <input class="form-control" name="past-psw" placeholder="current password" type="password">
+
+                        <label class="form-group">New password:</label>
+                        <input class="form-control" name="new-psw" placeholder="new password" type="password">
+
+                        <div style="text-align: center"><br>
+                            <input class="form-group btn btn-warning" type="submit" value="Update...">
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <?php include('footer.php'); ?>
+</body>
+</html>
